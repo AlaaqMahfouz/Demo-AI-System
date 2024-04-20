3532a1074e508e21dac7cd895ab8c96df2f7d580
Jane Beyrouthy <148901515+jane-beyrouthy@users.noreply.github.com>
2024-04-20 09:14:33 +0300

    update search

--- my-app/server/search.ts
+++ my-app/server/search.ts
@@ -8,19 +8,9 @@ import { createClient } from "@supabase/supabase-js";
     then based on this query we search the database, and return the results as a list of candidates
 */
 
-const request = `
-Bachelor's degree in Computer Science, Software Engineering, or a related field
-Proven experience in software development, with a focus on back-end technologies
-Strong proficiency in programming languages such as Java, C++, or Python
-Knowledge of software development methodologies, tools, and frameworks
-Familiarity with databases, SQL, and data modeling
-Experience with version control systems, such as Git
-Understanding of Agile development methodologies and practices
-Excellent problem-solving and analytical skills
-Strong communication and interpersonal skills
-Ability to work collaboratively in a team environment
-`;
-
+//convertText takes the search string text and converts it into a structured JSON object defined inside the code
+//this function returns the JSON object as string to be then used search through the database using searchDatabase function
+//the result should be passed to a const variable to be also stored in the database after the user saves his search -> saveSearch
 export async function convertText(searchString:string): Promise<string> {
 
     if(isEmptyString(searchString))
@@ -108,55 +98,77 @@ export async function convertText(searchString:string): Promise<string> {
           return ''; 
     }
 }
-//searchDatabase(convertRequest(searchText))
-export async function searchDatabase(searchText: string, inputNumber: number): Promise<any[]> {
+
+//this function takes the structured search text as string and the number of results desired 
+//using the structured JSON object we perform the searching using a select query that passes through the entire database
+//we store the selected resumes' IDs in an array and make sure that we dont have duplicates ids
+//using this array we would select all the resumes' names
+//the return of this function is an array of JSON objects
+export async function searchDatabase(structuredSearchString: string, inputNumber: number): Promise<any[]> {
 
   //call convertText to structure the search text into a JSON format
-  const structuredString = await convertText(searchText);
+  //const structuredString = await convertText(searchText);
 
-  if(isEmptyString(structuredString))
+  if(isEmptyString(structuredSearchString))
     {
         console.error("search is null and couldn't be converted!");
         return [];
     }
   try {
-    const searchJSON = JSON.parse(structuredString);
+    //the convertText function returns JSON format as a string so we have to parse it into JSON
+    const searchJSON = JSON.parse(structuredSearchString);
 
     // Initialize Supabase client
     const client = createClient("https://oquytlezdjnnavnjwsue.supabase.co","eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9xdXl0bGV6ZGpubmF2bmp3c3VlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTExODQ2NTYsImV4cCI6MjAyNjc2MDY1Nn0.2_PfE7QWBKQmPmUKHaTGX_DtUNDTmXnkW8rkMsEfzcw");
-
-    const selectedResumes : number[] = []; //array of the selected resumes' IDs 
-    const tables = Object.keys(searchJSON); 
+    
+    const selectedResumes : number[] = []; //initializing the array of the selected resumes' IDs 
+    const tables = Object.keys(searchJSON); //the top-level keys of our JSON object matches the tables' titles we want to search through in the database
+    //we iterate through each table 
     for (const table of tables) {
-      const columns = Object.keys(searchJSON[table]);
+      const columns = Object.keys(searchJSON[table]); //the 2nd-level keys of the JSON object matches the columns of each table
+      //for each column we select the resume's ID at the row where the value in the database matches the value in the JSON object
       for(const column of columns){
+        //in some search cases we can have values as arrays in the JSON object which holds the search requirements 
         if (Array.isArray(searchJSON[table][column])) {
+          //so we have to perform the select query for each value of this array 
           for(const value of searchJSON[table][column]){
+            //the select query returns the resume ID of the row that matches the requirement making sure we don't match null values together
             const { data, error } = await client.from(table).select('resumeID').ilike(column,`%${value}%`).neq(column,null);
             if (error) {
+              //error handeling
               console.error(`Error searching ${table} at ${column}:`, error.message);
               break;
             } else {
-             for(const resume of data){
-              selectResume(selectedResumes, resume.resumeID);
+              //data is an array of JSON objects with resumeID as the key 
+              for(const resume of data){
+                //for each resume we make sure this resume was not selected previously using the selectResume function
+                selectResume(selectedResumes, resume.resumeID);
              } 
             }
           }
         } else {
+          //the case where the value in the JSON object are not an array
           const { data, error } = await client.from(table).select('resumeID').ilike(column,`%${searchJSON[table][column]}%`).neq(column, null);
-            if (error) {
+          if (error) {
+              //error handeling
               console.error(`Error searching ${table} at ${column}:`, error.message);
               break;
-            } else {
-             for(const resume of data){
+          } else {
+            //data is an array of JSON objects with resumeID as the key
+            for(const resume of data){
+              //for each resume we make sure this resume was not selected previously using the selectResume function
               selectResume(selectedResumes, resume.resumeID);
-             } 
-            }
+            } 
+          }
         }
       }
     }
-    const {data , error} = await client.from('resumes').select('name').in('resumeID',selectedResumes).limit(inputNumber);
+
+    //from the resume table we select the rows of the resumes that matches the search requirements 
+    //we make sure to limit the number of selected rows to the desired number of results and we order ascendently by their IDs
+    const {data , error} = await client.from('resumes').select().in('resumeID',selectedResumes).limit(inputNumber).order('resumeID');
     if(error){
+      //error handeling
       console.error('Error selecting resumes: ',error);
       return [];
     }else{
@@ -167,6 +179,11 @@ export async function searchDatabase(searchText: string, inputNumber: number): P
     console.error('Error searching through the database:', error);
     return [];
   }
+}
+
+export async function saveSearch(structuredSearchString:string, searchData: any[]) {
+
+  
 }
 
 function selectResume(selectedResumes: number[], ID: number){
