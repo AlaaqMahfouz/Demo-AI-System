b1d5c97d0982ee2c74cda3e5078b186fbfd24aa3
Jane Beyrouthy <148901515+jane-beyrouthy@users.noreply.github.com>
2024-04-28 11:06:25 +0300

    install supabase/auth-helpers-nextjs

--- my-app/client/.next/types/app/login/page.ts
+++ my-app/client/.next/types/app/login/page.ts
@@ -1,8 +1,8 @@
-// File: D:\GitHub\ai-recruitment-system-interns-ulf2-2024\my-app\client\src\app\home\page.tsx
-import * as entry from '../../../../src/app/home/page.js'
+// File: D:\Jane BEYROUTHY\VS Code\AI-Recruitment-System\my-app\client\src\app\login\page.tsx
+import * as entry from '../../../../src/app/login/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
-type TEntry = typeof import('../../../../src/app/home/page.js')
+type TEntry = typeof import('../../../../src/app/login/page.js')
 
 // Check that the entry is a valid entry
 checkFields<Diff<{
--- my-app/client/.next/types/app/page.ts
+++ my-app/client/.next/types/app/page.ts
@@ -1,4 +1,4 @@
-// File: D:\GitHub\ai-recruitment-system-interns-ulf2-2024\my-app\client\src\app\page.tsx
+// File: D:\Jane BEYROUTHY\VS Code\AI-Recruitment-System\my-app\client\src\app\page.tsx
 import * as entry from '../../../src/app/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
