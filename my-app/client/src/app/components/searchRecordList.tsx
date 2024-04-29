9a5800ae0b43a707e81ee692ac28881d83f881e1
Jane Beyrouthy <148901515+jane-beyrouthy@users.noreply.github.com>
2024-04-29 18:49:09 +0300

    commit 2

--- my-app/client/src/app/components/searchRecordList.tsx
+++ my-app/client/src/app/components/searchRecordList.tsx
@@ -3,6 +3,8 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { stringify } from 'querystring';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 
 interface SearchRecord {
   searchID: number,
@@ -10,6 +12,8 @@ interface SearchRecord {
 }
 
 const SearchRecordList: React.FC = () => {
+
+  const router = useRouter();
 
   const [searches, setSearches] = useState<SearchRecord[]>([]);
 
@@ -28,6 +32,13 @@ const SearchRecordList: React.FC = () => {
     getSearches();
   }, []);
 
+  const handleMouseEnter = () => {
+    for(const search of searches){
+      // Prefetch the search record page with the specified search ID and title
+      router.prefetch(`/search?searchID=${encodeURIComponent(search.searchID)}&title=${encodeURIComponent(search.title)}`);
+    }
+  };
+
   
     return (
       <div className="container mx-auto">
@@ -35,15 +46,19 @@ const SearchRecordList: React.FC = () => {
         <ul>
           {searches.length > 0 ? (searches.map((search) => (
             <li key={search.searchID} className="mb-2">
-              <span className="cursor-pointer flex items-center justify-center m-2 mb-10 rounded-3xl h-28  bg-gray-100 hover:bg-blue-950 text-blue-900 hover:text-gray-100 hover:shadow-xl hover:shadow-blue-900 hover:-translate-y-3 font-bold text-center text-lg border-blue-950 hover:border-gray-100  border-2">
-                {search.title}
-              </span>
+              <Link href={`/search?searchID=${encodeURIComponent(search.searchID)}&title=${encodeURIComponent(search.title)}`} as={`/searchRecord`} onMouseEnter={handleMouseEnter} passHref>
+                <span className="cursor-pointer flex items-center justify-center m-2 mb-10 rounded-3xl h-28  bg-gray-100 hover:bg-blue-950 text-blue-900 hover:text-gray-100 hover:shadow-xl hover:shadow-blue-900 hover:-translate-y-3 font-bold text-center text-lg border-blue-950 hover:border-gray-100  border-2">
+                  {search.title}
+                </span>
+              </Link>
             </li>
           ))) : (
             <li>
-              <span className="flex items-center justify-center m-2 mb-10 rounded-3xl h-28  bg-gray-100 text-blue-900 hover:shadow-xl hover:shadow-blue-900  font-bold text-center text-lg border-blue-950 hover:border-gray-100  border-2">
-                0 Saved Searches 
-              </span>
+              <Link href="/newSearch" passHref>
+                <span className="cursor-pointer flex items-center justify-center m-2 mb-10 rounded-3xl h-28  bg-gray-100 hover:bg-blue-950 text-blue-900 hover:text-gray-100 hover:shadow-xl hover:shadow-blue-900 hover:-translate-y-3 font-bold text-center text-lg border-blue-950 hover:border-gray-100  border-2">
+                  0 Saved Searches 
+                </span>
+              </Link>
             </li>
           )}
           
---
+++ my-app/client/src/app/searchRecord/page.tsx
@@ -0,0 +1,124 @@
+'use client'
+
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import SearchResults from '../components/searchResults';
+import GoHomeHeadband from '../components/goHomeHeadband';
+import { redirect, useSearchParams } from 'next/navigation';
+import { parse, stringify } from 'querystring';
+
+interface SearchRecord{
+  searchID: number ;
+  title: string | "aaa";
+}
+
+const SearchFormAgain: React.FC = () => {
+  const [searchRecord, setSearchRecord] = useState<SearchRecord>();
+  const [isLoading, setIsLoading] = useState(true);
+  const searchParams = useSearchParams();
+  
+  const [limit, setLimit] = useState<number>(5);
+
+  useEffect(() =>{
+    const getSearchRecord = async () => {
+    // Access specific query parameters using get()
+    const searchID = searchParams.get('searchID');
+    const title = searchParams.get('title');
+    setSearchRecord({
+      searchID: searchID ? parseInt(searchID): 0,
+      title: title ? title: "aaa",
+    });
+    setIsLoading(false);
+    };
+    getSearchRecord();
+  },[])
+  if (isLoading) {
+    return <div>Loading...</div>
+  }
+
+  const handleSearch = async () => {
+    try {
+      const response = await axios.post('http://localhost:4000/new-search', {
+        limit
+      });
+
+    //   setSearchResults(response.data);
+    } catch (error) {
+      console.error('Error performing search:', error);
+    }
+  };
+
+//   const handleSaveSearch = async () => {
+//     try {
+//       // Make HTTP POST request to save the search in the database
+//       const response = await axios.post('/api/save-search', {
+//         searchTitle,
+//         structuredSearchString, 
+//         searchResults // Pass the search results to be saved
+//       });
+
+//       console.log('Search saved successfully:', response.data);
+//       setSavedSearch(true); // Set savedSearch to true after saving
+//     } catch (error) {
+//       console.error('Error saving search:', error);
+//     }
+//   };
+
+  const handleCancel = {
+    handleClick: () => {
+      redirect("/home");
+    },
+};
+
+  return (
+    <div>
+      <GoHomeHeadband/>
+      <div className='m-7 font-mono font-bold'>
+        <div className='text-6xl text-blue-900 text-center m-12'>
+          {/*{searchRecord?.title} with ID= {searchRecord?.searchID}*/}
+          Search
+        </div>
+
+        <div className="flex items-center justify-center h-full">
+          <div className="align-center">
+            <label htmlFor="resultNumber" className="mb-2 text-sm font-medium text-indigo-500 mr-3 dark:text-white"></label>
+            <div className="flex">
+              <p className="text-blue-900 font-bold p-4">
+                Number Of New Candidates Needed =
+              </p>
+              <input type="number" value={limit} onChange={(e) => setLimit(parseInt(e.target.value))} id="resultNumber" name="resultNumber" defaultValue='5' min="1" max="20" className="block ps-4 text-sm bg-gray-200 text-center text-blue-900 border-2 border-blue-900 focus:border-3 placeholder:text-gray-500 rounded-xl"  placeholder="5"/>
+            </div>
+          </div>
+        </div>
+        <div className='mt-5 mb-7 button-container'>
+          <button 
+            onClick={handleSearch}
+            className="flex m-4 justify-center text-lg bg-blue-800 hover:bg-blue-900 text-white shadow-lg shadow-gray-500 font-extrabold h-min w-15 py-3 px-4 rounded-full"
+          >
+            Search Again
+            <div className="p-1 ps-3 inset-y-0 start-0 flex items-center pointer-events-none">
+              <svg className="w-4 h-4" aria-hidden="true" fill="none" viewBox="0 0 20 20">
+                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+              </svg>
+            </div>
+          </button>
+        </div>
+          
+        <hr className="max-w mx-8 mt-4 border-blue-950 border-2 m-12"/>
+        {/* Render the SearchResults component and pass the searchResults state */}
+        {/* <SearchResults results={searchResults} /> */}
+
+        {/* Render the "Save" button if search results are present and not already saved */}
+        {/* {searchResults.length > 0 && !savedSearch && (
+          <div className='flex flex-row-reverse space-x-4 space-x-reverse mt-7'>
+            <button onClick={handleSaveSearch} className='flex m-4 justify-center text-lg bg-blue-800 hover:bg-blue-900 text-white shadow-lg shadow-gray-500 font-extrabold h-min w-15 py-3 px-4 rounded-full'>Save Search</button>
+            <button onClick={handleCancel.handleClick} className="w-full p-3 rounded-full bg-gray-500 text-white hover:bg-gray-700 focus:outline-none"> Cancel </button>
+          </div>
+          
+        )} */}
+      </div>
+    </div>
+  );
+};
+
+export default SearchFormAgain;
