75c095abe85e44e67bc030e865fe6b2905770d23
Jane Beyrouthy <148901515+jane-beyrouthy@users.noreply.github.com>
2024-05-11 15:08:18 +0300

    url component

--- my-app/client/src/app/components/searchRecordList.tsx
+++ my-app/client/src/app/components/searchRecordList.tsx
@@ -2,24 +2,24 @@
 
 import React, { useState, useEffect, Suspense } from 'react';
 import axios from 'axios';
-import { stringify } from 'querystring';
 import Link from 'next/link';
+import UrlComponent from './urlComponent';
 
-interface SearchRecord {
+interface SearchRecordDetails {
   searchID: number,
   title: string;
 }
 
 const SearchRecordList: React.FC = () => {
 
-  const [searches, setSearches] = useState<SearchRecord[]>([]);
+  const [searches, setSearches] = useState<SearchRecordDetails[]>([]);
   
   useEffect(() => {
     const getSearches = async () => {
       try {
         const response = await axios.get('http://localhost:4000/get-searches');
         
-        const searchesList: SearchRecord[] =  await response.data;
+        const searchesList: SearchRecordDetails[] =  await response.data;
         setSearches(searchesList);
       } catch (error) {
         console.error(error);
@@ -38,17 +38,7 @@ const SearchRecordList: React.FC = () => {
           {searches.length > 0 ? (searches.map((search) => (
 
             <li key={search.searchID} className="mb-2">
-              <Link href={{
-                pathname: '/searchRecord',
-                query:{
-                  searchID: search.searchID,
-                  title: search.title
-                }
-              }}  passHref>
-                <span className="cursor-pointer flex items-center justify-center m-2 mb-10 rounded-3xl h-28  bg-gray-100 hover:bg-blue-950 text-blue-900 hover:text-gray-100 hover:shadow-xl hover:shadow-blue-900 hover:-translate-y-3 font-bold text-center text-lg border-blue-950 hover:border-gray-100  border-2">
-                  {search.title}
-                </span>
-              </Link>
+              <UrlComponent searchID={search.searchID} title={search.title}/>
             </li>
           ))) : (
             <li>
---
+++ my-app/client/src/app/components/urlComponent.tsx
@@ -0,0 +1,25 @@
+import Link from 'next/link'
+import React from 'react'
+
+interface UrlComponentProps{
+    searchID: number,
+    title: string
+}
+
+const UrlComponent:React.FC<UrlComponentProps> = ({searchID, title}) => {
+  return (
+    <Link href={{
+        pathname: '/searchRecord',
+        query:{
+          searchID: searchID,
+          title: title
+        }
+      }}  passHref>
+        <span className="cursor-pointer flex items-center justify-center m-2 mb-10 rounded-3xl h-28  bg-gray-100 hover:bg-blue-950 text-blue-900 hover:text-gray-100 hover:shadow-xl hover:shadow-blue-900 hover:-translate-y-3 font-bold text-center text-lg border-blue-950 hover:border-gray-100  border-2">
+          {title}
+        </span>
+      </Link>
+  )
+}
+
+export default UrlComponent
