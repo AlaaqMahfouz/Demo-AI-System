2dde84a37c02a4007e96ac9c46d9d4ee4954c6d6
Alaa Mahfouz <155734527+AlaaqMahfouz@users.noreply.github.com>
2024-05-13 13:42:44 +0300

    pl

--- my-app/pages/utils functions/newSearch.ts
+++
@@ -1,6 +0,0 @@
-import { searchDatabase } from './searchDatabase';
-
-export async function newSearch(structuredSearchString: string, inputNumber: number): Promise<any[]> {
-  console.log("string parsed in new search :" + structuredSearchString)
-    return searchDatabase(structuredSearchString, inputNumber, [])
-  }
--- my-app/pages/utils functions/selectResume.ts
+++
@@ -1,6 +0,0 @@
-export function selectResume(selectedResumes: number[], ID: number){
-    //to prevent duplicate IDs in the selectedResumes array 
-    if (!selectedResumes.includes(ID)) {
-      selectedResumes.push(ID);
-    }  
-}
