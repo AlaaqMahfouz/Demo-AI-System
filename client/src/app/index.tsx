5dd46ad3a9a37a0b4f16b2aef3b9aefd203adcc5
Alaa Mahfouz <155734527+AlaaqMahfouz@users.noreply.github.com>
2024-05-14 08:57:45 +0300

    trying index.ts

--- client/src/app/index.tsx
+++ client/src/app/index.tsx
@@ -7,19 +7,11 @@ export default async function Login() {
   const cookieStore = cookies();
   const supabase = createServerComponentClient({cookies: () => cookieStore});
 
-  const {data: {user}} = await supabase.auth.getUser()
-  
-  if (!user){
-    return (
-      <main className="bg-gray-300 flex min-h-screen flex-col items-center justify-center p-24">
-        <Link href={'/login'} className='bg-blue-800 hover:bg-blue-900 rounded-full text-white p-5'>
-          You are not logged in.<br></br> <b>CLICK HERE</b> to go login.
-        </Link>
-      </main>
-    )
-  }
+ 
 
   return (
-    redirect("/home")
+    <div>
+        <p> hello to AI</p>
+    </div>
   )
 }
