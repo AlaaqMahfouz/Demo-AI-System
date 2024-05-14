f231c5f2d11419c5beaa749aaf651f4e85ee08bb
Alaa Mahfouz <155734527+AlaaqMahfouz@users.noreply.github.com>
2024-05-14 09:27:50 +0300

    ss

--- client/index.tsx
+++ client/index.tsx
@@ -7,11 +7,19 @@ export default async function Login() {
   const cookieStore = cookies();
   const supabase = createServerComponentClient({cookies: () => cookieStore});
 
- 
+  const {data: {user}} = await supabase.auth.getUser()
+  
+  if (!user){
+    return (
+      <main className="bg-gray-300 flex min-h-screen flex-col items-center justify-center p-24">
+        <Link href={'/login'} className='bg-blue-800 hover:bg-blue-900 rounded-full text-white p-5'>
+          You are not logged in.<br></br> <b>CLICK HERE</b> to go login.
+        </Link>
+      </main>
+    )
+  }
 
   return (
-    <div>
-        <p> hello to AI</p>
-    </div>
+    redirect("/home")
   )
 }
